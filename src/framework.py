82285aa498ac1331148085b10e9b46de62c831a1
Rilow <rilowgg@gmail.com>
2022-02-06 23:21:32 -0500

    add typed to core framework

--- src/framework.py
+++ src/framework.py
@@ -7,16 +7,33 @@ All modules depend on this.
 """
 from typing import Set
 
+# Because typed is not part of the framework
+# it is not guarenteed to be available.
+try:
+	from typed import typed
+except ImportError:
+	def typed(x): return x
+
 __DEFINES__: Set[str] = set()
 
+@typed
 def define(x: str) -> None:
 	"""
 	Define `x` marking it as true.
 	"""
 	__DEFINES__.add(x.lower())
 
+@typed
 def defined(x: str) -> bool:
 	"""
 	Returns True if `x` is defined.
 	"""
 	return x.lower() in __DEFINES__
+
+# Normally modules that are part of the framework will define themselves
+# but because typed is imported by the framework to avoid circular imports
+# it is defined here. We can check if the module for the typed
+# function defined at the tope of this file is of the "typed" module
+# and not "__main__" or "framework"
+if typed.__module__ == "typed":
+	define("TYPED_PY")
